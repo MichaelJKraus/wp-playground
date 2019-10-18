a65f26d67b4a5ee197eea264ba42fa39cf09eb57
Michael Kraus <michaelkraus@Booger-Hook-on-the-Bang-Switch.local>
2019-10-18 13:55:40 -0700

    Added JS file and folder to project

--- wp-content/themes/mytheme/functions.php
+++ wp-content/themes/mytheme/functions.php
@@ -1,5 +1,11 @@
 <?php 
-    function register_my_menus() {
+   function add_my_scripts() {
+        wp_enqueue_style('my_stylesheet', get_stylesheet_uri());
+        wp_enqueue_script('my_javascript', get_stylesheet_directory_uri(). '/js/index.js');
+   }
+   add_action('wp_enqueue_scripts', 'add_my_scripts');
+   
+   function register_my_menus() {
         register_nav_menus(
             array(
                 'header-menu' => __( 'Header Menu '),
--- wp-content/themes/mytheme/header.php
+++ wp-content/themes/mytheme/header.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>My First Custom Theme</title>
-    <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>" type="text/css">
+    
     <?php wp_head(); ?>
 </head>
 <body>
