4dd9c0c67b3b55ba80c9bc4940f67848b030e265
Michael Kraus <michaelkraus@Booger-Hook-on-the-Bang-Switch.local>
2019-10-24 15:28:15 -0700

    Created Plugin Folder and File

---
+++ wp-content/plugins/wpuc-plugin-sorter/wpuc-plugin-sorter.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * Plugin Name: WordPress Urgent Care Plugin Sorter
+ * Plugin URI: https://www.wordpressurgentcare.com
+ * Description: WordPress Urgent Cares first in-house custom plugin. The plugin is
+ * designed to sort and filter WordPress Pligins 
+ * Version: 1.0
+ * Author: Michael J. Kraus
+ * Author URI: http://www.worpressurgentcare.com
+ */
